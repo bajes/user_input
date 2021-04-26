9a5519b9cdc3b504909834befa5e70db1e295077
bajes <bajes@users.noreply.github.com>
2021-04-26 10:09:44 +0300

    first commit

---
+++ lib/Pages/lecture_test.dart
@@ -0,0 +1,54 @@
+﻿import 'package:flutter/material.dart';
+
+enum Gender { male, female }
+
+class LectureTest extends StatefulWidget {
+  @override
+  _LectureTestState createState() => _LectureTestState();
+}
+
+class _LectureTestState extends State<LectureTest> {
+  Gender gender = Gender.male;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'user name'),
+            ),
+            Row(
+              children: [
+                Radio(
+                    value: Gender.male,
+                    groupValue: gender,
+                    onChanged: (Gender value) {
+                      this.gender = value;
+                      setState(() {});
+                    }),
+                Text('Male')
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                    value: Gender.female,
+                    groupValue: gender,
+                    onChanged: (Gender value) {
+                      this.gender = value;
+                      setState(() {});
+                    }),
+                Text('FeMale')
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:radio_button/Pages/lecture_test.dart';
+
+void main() {
+  runApp(MaterialApp(home: LectureTest()));
+}
